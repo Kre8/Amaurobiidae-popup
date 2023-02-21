567d92c0372d8af0fdd1d2e8436fdb737a3c85a3
L-Turnbull <messagelyn@gmail.com>
2023-02-21 14:59:36 +1100

    4
    4

--- am/tooltip.js
+++ am/tooltip.js
@@ -1,40 +1,40 @@
-//  js file
+
 
 let toolTip = document.getElementById("tooltip3");
 let copy = document.getElementById("copy");
 let ttImages = document.getElementsByClassName("tt-image");
 
 const track = () => {
-  //  run function on mouse move
-  let mouseX = event.clientX + document.body.scrollLeft + 1; //  get x position of mouse
-  let mouseY = event.clientY + document.body.scrollTop - 40; //  get y position of mouse
-  toolTip.setAttribute("style", "top:" + mouseY + "px; left:" + mouseX + "px;"); //  set the x and y of tooltip based on mouse position
+
+  let mouseX = event.clientX + document.body.scrollLeft + 1; 
+  let mouseY = event.clientY + document.body.scrollTop - 40; 
+  toolTip.setAttribute("style", "top:" + mouseY + "px; left:" + mouseX + "px;"); on mouse position
 };
-window.addEventListener("mousemove", track); //  listen for mouse movement
+window.addEventListener("mousemove", track); 
 
 const showTt = (e) => {
   if (e.target.tagName === "SPAN") {
-    //  test if hovered element is a span
-    toolTip.setAttribute("class", "show"); //  show the tooltip
-    let ttImage = e.target.dataset.img; //  get the data attribute of hovered span
-    toolTip.innerHTML = '<img src="' + ttImage + '">'; //  populate the tooltip with an image tag + retrieved data attribute
+
+    toolTip.setAttribute("class", "show"); 
+    let ttImage = e.target.dataset.img; 
+    toolTip.innerHTML = '<img src="' + ttImage + '">'; attribute
   } else {
-    toolTip.setAttribute("class", ""); //  hide the tooltip
+    toolTip.setAttribute("class", ""); 
   }
 };
-copy.addEventListener("mouseover", showTt); //  copy hover function
+copy.addEventListener("mouseover", showTt); 
 
-//  preload the images
+
 
 let allCities = [];
 let cities = [];
 
 for (let i = 0; i < ttImages.length; i++) {
-  allCities.push(ttImages[i].dataset.img);  //  build array of img urls
+  allCities.push(ttImages[i].dataset.img);  
 }
 
 for (let i = 0; i < allCities.length; i++) {
-  //  loop through the cities array
+ 
   cities[i] = new Image();
   cities[i].src = allCities[i];
 }
